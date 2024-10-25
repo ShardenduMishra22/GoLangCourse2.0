7ae13da0b11e10c4cb3e05eb95dbcb5e316e6f85
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-25 23:43:11 +0530

    Learning GoLang

--- 8thBasic/main.go
+++ 8thBasic/main.go
@@ -7,15 +7,23 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 
-	var num int
-	fmt.Scan(&num)
-	fmt.Println("You entered: ", num)
+	// var num int
+	// fmt.Scan(&num)
+	// fmt.Println("You entered: ", num)
 
-	if num > 18 {
-		fmt.Println("You are an adult")
-	} else if num == 18 {
-		fmt.Println("Not Yet")
+	// if num > 18 {
+	// 	fmt.Println("You are an adult")
+	// } else if num == 18 {
+	// 	fmt.Println("Not Yet")
+	// } else {
+	// 	fmt.Println("You are a child")
+	// }
+
+	x := 1
+	y := 7
+	if x > 10 || (y > 5 && y < 10) {
+		fmt.Println("Good")
 	} else {
-		fmt.Println("You are a child")
+		fmt.Println("Bad")
 	}
 }
