003be3d2f608ec66780318a05a44eda708fd02df
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-26 00:44:50 +0530

    Leanring GoLang

--- 14thBasic/main.go
+++ 14thBasic/main.go
@@ -3,8 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var num int = 10
-	var ptr *int = &num
+	// var num int = 10
+	// var ptr *int = &num
+
+	// fmt.Println("num: ", num)
+	// fmt.Println("ptr: ", ptr)
+	// fmt.Println("Value inside the ptr (same as num): ", *ptr)
+
+	num := 10
+	ptr := &num
 
 	fmt.Println("num: ", num)
 	fmt.Println("ptr: ", ptr)
