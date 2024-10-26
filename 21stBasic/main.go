e2b8335215fed93ba6dffbabbeb4250dafc86ab8
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-10-26 20:56:19 +0530

    Learning GoLang

--- 21stBasic/main.go
+++ 21stBasic/main.go
@@ -44,7 +44,79 @@ type ToDo struct {
 	Completed bool   `json:"completed"`
 }
 
-// Function to make a PATCH request
+func makeGetRequest() {
+	fmt.Println("GET Request")
+
+	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
+	HandleError(err)
+	defer res.Body.Close()
+
+	if res.StatusCode >= 300 {
+		fmt.Println("Error: ", res.StatusCode)
+		return
+	}
+
+	var todo ToDo
+	err = json.NewDecoder(res.Body).Decode(&todo)
+	HandleError(err)
+
+	fmt.Println("Todo: ", todo)
+}
+
+func makePostRequest() {
+	fmt.Println("POST Request")
+
+	todo := ToDo{
+		UserId:    1,
+		Title:     "Dollar Sign One Time",
+		Completed: false,
+	}
+
+	jsonData, err := json.Marshal(todo)
+	HandleError(err)
+
+	jsonString := string(jsonData)
+	jsonReader := strings.NewReader(jsonString)
+
+	myURL := "https://jsonplaceholder.typicode.com/todos"
+
+	res, err := http.Post(myURL, "application/json", jsonReader)
+	HandleError(err)
+	defer res.Body.Close()
+
+	fmt.Println("Status Code: ", res.StatusCode)
+}
+
+// we have to create a put reqest it doesent qutomatically exist
+func makePutRequest() {
+	todo := ToDo{
+		UserId:    1,
+		Id:        1,
+		Title:     "delectus aut autem",
+		Completed: false,
+	}
+
+	jsonData, err := json.Marshal(todo)
+	HandleError(err)
+
+	jsonString := string(jsonData)
+	jsonReader := strings.NewReader(jsonString)
+	myURL := "https://jsonplaceholder.typicode.com/todos/1"
+
+	req, err := http.NewRequest(http.MethodPut, myURL, jsonReader)
+	HandleError(err)
+
+	req.Header.Set("Content-Type", "application/json; charset=utf-8")
+	client := http.Client{}
+	res, err := client.Do(req)
+	HandleError(err)
+	defer res.Body.Close()
+	
+	data, _ := io.ReadAll(io.Reader(res.Body))
+	fmt.Println(string(data))
+}
+
+// Function to make a PATCH request it doesent qutomatically exist
 func makePatchRequest() {
 	updatedData := map[string]interface{}{
 		"title": "Todo One Updated",
@@ -80,78 +152,6 @@ func makeDeleteRequest() {
 	defer res.Body.Close()
 
 	fmt.Println("Delete Request Status Code:", res.StatusCode)
-}
-
-// we have to create a put reqest it doesent qutomatically exist
-func makePutRequest() {
-	todo := ToDo{
-		UserId:    1,
-		Id:        1,
-		Title:     "delectus aut autem",
-		Completed: false,
-	}
-
-	jsonData, err := json.Marshal(todo)
-	HandleError(err)
-
-	jsonString := string(jsonData)
-	jsonReader := strings.NewReader(jsonString)
-	myURL := "https://jsonplaceholder.typicode.com/todos/1"
-
-	req, err := http.NewRequest(http.MethodPut, myURL, jsonReader)
-	HandleError(err)
-
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-	client := http.Client{}
-	res, err := client.Do(req)
-	HandleError(err)
-	defer res.Body.Close()
-
-	data, _ := io.ReadAll(io.Reader(res.Body))
-	fmt.Println(string(data))
-}
-
-func makePostRequest() {
-	fmt.Println("POST Request")
-
-	todo := ToDo{
-		UserId:    1,
-		Title:     "Dollar Sign One Time",
-		Completed: false,
-	}
-
-	jsonData, err := json.Marshal(todo)
-	HandleError(err)
-
-	jsonString := string(jsonData)
-	jsonReader := strings.NewReader(jsonString)
-
-	myURL := "https://jsonplaceholder.typicode.com/todos"
-
-	res, err := http.Post(myURL, "application/json", jsonReader)
-	HandleError(err)
-	defer res.Body.Close()
-
-	fmt.Println("Status Code: ", res.StatusCode)
-}
-
-func makeGetRequest() {
-	fmt.Println("GET Request")
-
-	res, err := http.Get("https://jsonplaceholder.typicode.com/todos/1")
-	HandleError(err)
-	defer res.Body.Close()
-
-	if res.StatusCode >= 300 {
-		fmt.Println("Error: ", res.StatusCode)
-		return
-	}
-
-	var todo ToDo
-	err = json.NewDecoder(res.Body).Decode(&todo)
-	HandleError(err)
-
-	fmt.Println("Todo: ", todo)
 }
 
 func HandleError(err error) {
